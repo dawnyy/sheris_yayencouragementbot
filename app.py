5ef98bc561dec1d9c1a21986a98bed8e300d8f1c
shijiax <69945072+shijiax@users.noreply.github.com>
2020-10-20 15:04:40 +0800

    Update app.py
    I added more feelings in the code so that there is a wider variety of feelings that it can recognise.

--- app.py
+++ app.py
@@ -13,20 +13,23 @@ while True:
     
     if each_word == "sad":
       feelings_list.append("sad")
-      encouragement_list.append("tomorrow will be better,  feeling sad is being human")
+      encouragement_list.append("tomorrow will be better :> It is okay to not be okay, feeling sad just means you are human")
       counter += 1
     if each_word == "happy":
       feelings_list.append("happy")
-      encouragement_list.append("to keep smiling, live life happily and make every moment your best part")
+      encouragement_list.append("you must keep smiling, live life happily and live every moment like it's your last")
       counter += 1
     if each_word == "tired":
       feelings_list.append("tired")
-      encouragement_list.append("you are stronger than you think, dont give up, life is like a marathon and its okay to be slow")
+      encouragement_list.append("you are stronger than you think, dont give up! Life is like a marathon and it is okay to be slow")
       counter += 1
-      
     if each_word == "bored":
       feelings_list.append("bored")
-      encouragement_list.append("Go ocupy yourself, spend time with you loved ones. Life is short, make the most of it.")
+      encouragement_list.append("you should go ocupy yourself, spend time with you loved ones. Life is short, make the most of it")
+      counter += 1
+    if each_word == "angry":
+      feelings_list.append("angry")
+      encouragement_list.append("why spend time being angry at something or someone when you can live life to the fullest? Enjoy life")
       counter += 1
 
   if counter == 0:
